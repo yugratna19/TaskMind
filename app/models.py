d687b1afd9ed8309c38b6aa1675e053c92929bc2
Yugratna Humagain <yugratna59@gmail.com>
2025-01-01 14:01:13 +0545

    Tasks in admin pannel

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title =  models.CharField(max_length= 200)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    
+    def __str__(self):
+            return self.title
