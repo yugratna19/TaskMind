8bb0e618561bb0faaa1f6f10d17f413f6ee0b7f6
Yugratna Humagain <yugratna59@gmail.com>
2025-01-01 15:02:01 +0545

    Basic backend setup and task lists

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home route
+    path('tasks/', views.task_list, name = 'task_list' ), # Task list route
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def task_list (request):
+    tasks = [
+        {"title": "Study Django", "description": "Work on the TaskMind project", "due_date": "2025-01-02"},
+        {"title": "Workout", "description": "Follow workout routine", "due_date": "2025-01-02"},
+    ]
+    return render(request, 'task.html',{'tasks':tasks}) 
