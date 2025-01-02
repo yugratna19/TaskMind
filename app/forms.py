4db57b3ca1654daf8ddf565354fdf9c350d82772
Yugratna Humagain <yugratna59@gmail.com>
2025-01-02 13:14:49 +0545

    Add calander to due_date field

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,6 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'due_date', 'status']
+        widgets = {
+            'due_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        }
