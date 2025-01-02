11aebbd4843e198b1ecc679acf63c9d8089ba2b0
Yugratna Humagain <yugratna59@gmail.com>
2025-01-02 12:45:00 +0545

    Add urls.py for add and edit forms

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home route
-    path('tasks/', views.task_list, name = 'task_list' ), # Task list route
+    path('tasks/', views.task_list, name = 'task_list'), # Task list route
+    path('tasks/add', views.add_task, name = 'add_task'),
+    path('tasks/edit/<int:id>', views.edit_task, name = 'edit_task'),
 ]
