307f6b64acb865a8323507773cf69ef638095d17
Yugratna Humagain <yugratna59@gmail.com>
2025-01-02 12:49:59 +0545

    Modify views.py for prefilled data in edit task

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
 from .forms import TaskForm
 
@@ -23,11 +23,12 @@ def add_task(request):
     return render (request, 'add_task.html',{'form': form})
 
 def edit_task(request,id):
-    task = Task.objects.get(id =id)
+    task = get_object_or_404(Task,id = id)
     if request.method == 'POST':
         form = TaskForm(request.POST, instance= task)
         if form.is_valid():
             form.save()
+            return redirect('task_list')
     else: 
         form = TaskForm(instance= task)
     return render (request, 'edit_task.html',{'form': form})
