f8ecbdd1e74b2097a0d676267f42c65a0fa0a4cd
Yugratna Humagain <yugratna59@gmail.com>
2025-01-02 12:41:38 +0545

    Create view for add and edit form

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def home(request):
@@ -10,3 +12,23 @@ def task_list (request):
         {"title": "Workout", "description": "Follow workout routine", "due_date": "2025-01-02"},
     ]
     return render(request, 'task.html',{'tasks':tasks}) 
+
+def add_task(request):
+    if request.method == 'Post':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else: 
+        form = TaskForm()
+    return render (request, 'add_task.html',{'form': form})
+
+def edit_task(request,id):
+    task = Task.objects.get(id =id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance= task)
+        if form.is_valid():
+            form.save()
+    else: 
+        form = TaskForm(instance= task)
+    return render (request, 'edit_task.html',{'form': form})
+
