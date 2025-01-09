67f7880460b1988d4269cfa0140ce410ef93f0d0
Yugratna Humagain <yugratna59@gmail.com>
2025-01-09 12:01:51 +0545

    Add priority field in models

--- app/models.py
+++ app/models.py
@@ -7,12 +7,22 @@ class Task(models.Model):
         (NOT_COMPLETED, 'Not Completed'),
         (COMPLETED, 'Completed'),
     ]
+    
+    HIGH = 'High'
+    MEDIUM = 'Medium'
+    LOW = 'Low'
+    PRIORITY_CHOICES = [
+        (HIGH, 'High'),
+        (MEDIUM, 'Medium'),
+        (LOW, 'Low'),
+    ]
 
     id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     due_date = models.DateField(blank=True, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=NOT_COMPLETED)
+    priority = models.CharField(max_length=10, choices= PRIORITY_CHOICES, default= MEDIUM) 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     
