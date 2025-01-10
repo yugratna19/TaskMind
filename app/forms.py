3d80b4165d4f3cb0f1540c62770f29a9c368377c
Yugratna Humagain <yugratna59@gmail.com>
2025-01-10 17:44:53 +0545

    Task priority complete

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,8 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'due_date', 'status']
+        fields = ['title', 'description', 'due_date', 'status', 'priority']  # Add 'priority' to fields
         widgets = {
-            'due_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'due_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),  # Date picker for due_date
+            'priority': forms.Select(attrs={'class': 'form-control'})  # Dropdown for priority
         }
